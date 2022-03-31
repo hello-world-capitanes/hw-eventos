9d61181b963d42af681ff00676d896bac144c7b7
Mónica Rodríguez Pérez <monicarodriguez@devanddel.com>
2022-03-31 12:12:34 +0200

    Main logo

--- hw-eventos/src/app/features/main/main.component.ts
+++ hw-eventos/src/app/features/main/main.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
+  private static num_loadings = 0;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  firstLoad(){
+    console.log("firstLoad: " + !MainComponent.num_loadings);
+    return MainComponent.num_loadings <2;
+  }
 }
