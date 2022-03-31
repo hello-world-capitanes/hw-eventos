842a565c7487fb0012c4eaa3bccd6e39486c3edf
DANIEL-PARDO\Daniel Pardo <danielpardo@devanddel.com>
2022-03-31 12:11:30 +0200

    Commit pagina inicio carrusel

--- hw-eventos/src/app/features/main/main.component.ts
+++ hw-eventos/src/app/features/main/main.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
+
+  slides = [
+    {
+      url: 'https://source.unsplash.com/1600x900/?nature,water'
+    },
+    {
+      url: 'https://source.unsplash.com/1600x1600/?nature,forest'
+    }
+  ]
 
   constructor() { }
 
